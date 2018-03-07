0402a363a168ee0e988c3e63c5a7f6b6519e75e2
Moncel Arnaud <nessdu43@hotmail.fr>
2018-03-07 22:06:02 +0100

    Add multiple floating button with animation on carte activity

--- app/src/main/java/com/note/gestion/gestionnotes/CarteActivity.java
+++ app/src/main/java/com/note/gestion/gestionnotes/CarteActivity.java
@@ -5,26 +5,81 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class CarteActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_carte);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private boolean m_isFabOpen = false;
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    private FloatingActionButton m_fab;
+    private FloatingActionButton m_fabAddItem;
+    private FloatingActionButton m_fabAddGroup;
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*DialogFragment addTableDialog = SimpleAddDialog.newInstance( 1 );
-                addTableDialog.show( getFragmentManager(), "addTableDialog");*/
-            }
-        });
+    /*private TextView m_labelAddItem;
+    private TextView m_labelAddGroup;*/
+
+    private Animation m_animClose;
+    private Animation m_animOpen;
+    private Animation m_animRotBack;
+    private Animation m_animRotFor;
+
+    private void animateflocationBtn(){
+        if( m_isFabOpen ){
+            m_fab.startAnimation( m_animRotBack );
+            //m_labelAddGroup.startAnimation( m_animClose );
+            m_fabAddGroup.startAnimation( m_animClose );
+            m_fabAddItem.startAnimation( m_animClose );
+            m_fabAddGroup.setClickable( false );
+            m_fabAddItem.setClickable( false );
+            m_isFabOpen = false;
+        } else {
+            m_fab.startAnimation( m_animRotFor );
+            //m_labelAddGroup.startAnimation( m_animOpen );
+            m_fabAddGroup.startAnimation( m_animOpen );
+            m_fabAddItem.startAnimation( m_animOpen );
+            m_fabAddGroup.setClickable( true );
+            m_fabAddItem.setClickable( true );
+            m_isFabOpen = true;
+        }
     }
 
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_carte );
+        Toolbar toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+
+        m_fab = findViewById( R.id.fab );
+        m_fab.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+                animateflocationBtn();
+            }
+        } );
+
+        //m_labelAddGroup = findViewById( R.id.label_add_group );
+        m_fabAddGroup = findViewById( R.id.fab_add_group );
+        m_fabAddGroup.setOnClickListener( new  View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+            }
+        } );
+
+        m_fabAddItem = findViewById( R.id.fab_add_item );
+        m_fabAddItem.setOnClickListener( new  View.OnClickListener() {
+            @Override
+            public void onClick( View view ) {
+            }
+        } );
+
+        m_animOpen = AnimationUtils.loadAnimation( getApplicationContext(), R.anim.fab_open );
+        m_animClose = AnimationUtils.loadAnimation( getApplicationContext(),R.anim.fab_close );
+        m_animRotFor = AnimationUtils.loadAnimation( getApplicationContext(),R.anim.rotate_forward );
+        m_animRotBack = AnimationUtils.loadAnimation( getApplicationContext(),R.anim.rotate_backward );
+    }
 }
--- app/src/main/java/com/note/gestion/gestionnotes/TableActivity.java
+++ app/src/main/java/com/note/gestion/gestionnotes/TableActivity.java
@@ -3,12 +3,9 @@ package com.note.gestion.gestionnotes;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.note.gestion.table.Table;
 
